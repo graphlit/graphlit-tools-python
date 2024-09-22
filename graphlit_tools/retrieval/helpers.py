c816295a68f2bbfd6b7f9c287fc7f3689b9db9a9
Kirk Marple <kirk@graphlit.com>
2024-09-22 16:24:50 -0700

    .

--- graphlit_tools/retrieval/helpers.py
+++ graphlit_tools/retrieval/helpers.py
@@ -51,10 +51,12 @@ def format_content(content, include_text: Optional[bool] = True) -> List[str]:
             results.append(f'**{link.link_type} Link:** {link.uri}')
 
     if include_text is True:
+        results.append('\n')
+
         if content.pages is not None:
             for page in content.pages:
                 if page.chunks is not None and len(page.chunks) > 0:
-                    results.append(f'### Page #{page.index + 1}')
+                    results.append(f'**Page #{page.index + 1}**:')
 
                     for chunk in page.chunks:
                         results.append(chunk.text)
@@ -63,14 +65,13 @@ def format_content(content, include_text: Optional[bool] = True) -> List[str]:
 
         if content.segments is not None:
             for segment in content.segments:
-                results.append(f'### Transcript Segment [{segment.start_time}-{segment.end_time}]')
+                results.append(f'**Transcript Segment [{segment.start_time}-{segment.end_time}]**:')
                 results.append(segment.text)
 
                 results.append('\n')
 
         if content.pages is None and content.segments is None:
             if content.markdown is not None:
-                results.append('### Text')
                 results.append(content.markdown)
 
                 results.append('\n')
